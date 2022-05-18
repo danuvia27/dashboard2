f07f4e49e985a7296f2070a7e706f1b16cd3ef5a
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-17 18:53:23 -0700

    created components folder

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [data, setData] = useState([{}]);
 
   useEffect(() => {
-    fetch("/")
+    fetch("/titles")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
@@ -12,7 +12,16 @@ function App() {
       });
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>Ayooo</h1>
+      {typeof data.title === "undefined" ? (
+        <p>loading...</p>
+      ) : (
+        data.title.map((title, i) => <p key={i}>{title}</p>)
+      )}
+    </div>
+  );
 }
 
 export default App;
