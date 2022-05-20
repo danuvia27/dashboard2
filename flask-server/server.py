9acd62d8e14fe0e047a22c46d4c15ffdf9f376b8
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-20 11:48:25 -0700

    updated server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,14 +2,30 @@ import feedparser
 from flask import Flask
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-FEED_URL="http://status.us.onelogin.com/pages/538511e2ce5cb97537000144/rss"
 CORS(app, resources={r"/api/*":{"origins":"*"}})
 app.config['CORS HEADERS'] = 'Content-Type'
-@app.route("/titles")
+URL_FEED ="http://status.us.onelogin.com/pages/538511e2ce5cb97537000144/rss"
+@app.route("/")
 @cross_origin()
 
-def titles():
-    return {"title": ["title1", "title2", "title3"]}
+def get_feed():
+    feed = feedparser.parse(URL_FEED)
+    first_article =feed['entries'][0]
+    return """<html>
+    <body>  
+           <h1>OneLogin Issues<h1>
+           <b>{0}<b> <br />
+           <i>{1}<i> <br />
+           <b>{2}<b> <br />
+    </body>
+    </html>""".format(first_article.get("title"),
+    first_article.get("link"),
+    first_article.get("updated"))      
+   
+        
+         
+
+           
     
 
 if __name__ == '__main__':
