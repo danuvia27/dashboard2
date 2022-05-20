fcedc63ab24e25e068a42ffb91344dd0b0bf83f1
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-20 11:49:21 -0700

    updated featuredinfo.js and css

--- client/src/components/FeaturedInfo/FeaturedInfo.js
+++ client/src/components/FeaturedInfo/FeaturedInfo.js
@@ -1,5 +1,15 @@
-import "./featuredinfo.css";
+import "./featuredInfo.css";
+import { AiOutlinePoweroff } from "react-icons/ai";
 
 export default function FeaturedInfo() {
-  return <div className="featured"></div>;
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">
+          General <AiOutlinePoweroff />
+        </span>
+        <div className="featuredInfoContainer"></div>
+      </div>
+    </div>
+  );
 }
