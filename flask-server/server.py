72e6ee69e6eba5a0a5b7caaafcd29edb0ee39873
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-24 13:33:18 -0700

    changed return from html to JSON

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,33 +1,26 @@
 import feedparser
-from flask import Flask
+from flask import Flask ,jsonify
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app, resources={r"/api/*":{"origins":"*"}})
 app.config['CORS HEADERS'] = 'Content-Type'
-URL_FEED ="http://status.us.onelogin.com/pages/538511e2ce5cb97537000144/rss"
-@app.route("/")
+URLS = ("https://status.ui.com/history.rss")
+@app.route("/feed", methods=['GET'])
 @cross_origin()
 
-def get_feed():
-    feed = feedparser.parse(URL_FEED)
-    first_article =feed['entries'][0]
-    return """<html>
-    <body>  
-           <h1>OneLogin Issues<h1>
-           <b>{0}<b> <br />
-           <i>{1}<i> <br />
-           <b>{2}<b> <br />
-    </body>
-    </html>""".format(first_article.get("title"),
-    first_article.get("link"),
-    first_article.get("updated"))      
+def get_feeds():
+    feed = feedparser.parse(URLS)
+    first_article = feed['entries'][0]
+    
+    return jsonify(title=first_article.title,
+         description=first_article.description,
+         link=first_article.link, 
+         updated=first_article.updated)    
+
+   
+
    
         
-         
-
-           
-    
-
 if __name__ == '__main__':
     app.run(debug=True)
 
