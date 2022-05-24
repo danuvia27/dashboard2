69cdc7920047a6f48d502b26699c305a2f34d2b9
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-24 13:33:41 -0700

    Updating app.js fetching backend data

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
+
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([{}]);
 
   useEffect(() => {
-    fetch("/titles")
+    fetch("/feed")
       .then((res) => res.json())
       .then((data) => {
         setData(data);
@@ -22,12 +23,13 @@ function App() {
       <div className="container">
         <Sidebar />
         <Home />
+
+        {typeof data.feed === "undefined" ? (
+          <p>Loading...</p>
+        ) : (
+          data.feed.map((data, i) => <p key={i}>{data}</p>)
+        )}
       </div>
-      {typeof data.title === "undefined" ? (
-        <p>loading...</p>
-      ) : (
-        data.title.map((title, i) => <p key={i}>{title}</p>)
-      )}
     </div>
   );
 }
