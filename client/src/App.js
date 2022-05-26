de965c16e7b59ba6c662d3cc9057bade66e9a2ae
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-25 18:56:22 -0700

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
-import Tables from "./components/Table/Tables";
-import Home from "./pages/home/Home";
+import { Tables } from "./components/Table/Tables";
+import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const [feed, setFeed] = useState([]);
+
+  useEffect(() => {
+    fetch("/feed").then((response) =>
+      response.json().then((data) => {
+        setFeed(data);
+      })
+    );
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div className="container">
         <Sidebar />
         <Home />
-        <Tables />
+        <Tables feed={feed} />
       </div>
     </div>
   );
