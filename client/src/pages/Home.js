337aeae11a894a6deb48544a4745638771a6fd6e
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-25 18:55:28 -0700

    created home.js and css again

---
+++ client/src/pages/Home.js
@@ -0,0 +1,9 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h2> home</h2>
+    </div>
+  );
+}
--- client/src/pages/home/Home.js
+++
@@ -1,5 +0,0 @@
-import "./home.css";
-
-export default function Home() {
-  return <div className="home"></div>;
-}
