fcc3b64301bfe18c188f501a5083261385d38097
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-05-26 22:22:07 -0700

    updated server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app, resources={r"/api/*":{"origins":"*"}})
 app.config['CORS HEADERS'] = 'Content-Type'
-URLS = ("https://status.ui.com/history.rss")
+URLS = ("http://status.us.onelogin.com/pages/538511e2ce5cb97537000144/rss")
 @app.route("/feed", methods=['GET'])
 @cross_origin()
 
@@ -13,12 +13,9 @@ def get_feeds():
     first_article = feed['entries'][0]
     
     return jsonify(title=first_article.title,
-         summary=first_article.summary,
          link=first_article.link, 
-         updated=first_article.updated, 
-         description= first_article.description,
-         id= first_article.id) 
-         
+         updated=first_article.updated)
+        
 if __name__ == '__main__':
     app.run(debug=True)
 
