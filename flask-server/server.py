5e255bbbe798d87d7c3d5d74e47e322c818f0fe8
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-06 18:15:58 -0700

    added URL route to feed_ol

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,19 +3,39 @@ from flask import Flask ,jsonify
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app, resources={r"/api/*":{"origins":"*"}})
-app.config['CORS HEADERS'] = 'Content-Type'
-URLS = ("http://status.us.onelogin.com/pages/538511e2ce5cb97537000144/rss")
-@app.route("/feed", methods=['GET'])
+app.config['CORS HEADERS'] = 'Content-Type' 
+URLS =['https://status.ui.com/history.rss',
+'http://status.us.onelogin.com/pages/538511e2ce5cb97537000144/rss']
+
+
+@app.route("/feed", methods=['POST','GET'])
 @cross_origin()
 
-def get_feeds():
-    feed = feedparser.parse(URLS)
+def get_feeds_ui():
+    feed = feedparser.parse(URLS[0])
     first_article = feed['entries'][0]
     
     return jsonify(title=first_article.title,
          link=first_article.link, 
          updated=first_article.updated)
-        
+         
+@app.route("/feed_ol", methods=['POST','GET'])
+def get_feeds_ol():
+    feed = feedparser.parse(URLS[1])  
+    first_article = feed['entries'][0]
+
+    return jsonify(title=first_article.title,
+    link=first_article.link,
+    updated=first_article.updated)
+  
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
 
