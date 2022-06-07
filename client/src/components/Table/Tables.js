b091b228dfa6e28158f7d050b9cfaa8ca077b61a
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-06 18:14:14 -0700

    Updated table.js

--- client/src/components/Table/Tables.js
+++ client/src/components/Table/Tables.js
@@ -3,9 +3,57 @@ import * as ReactBootStrap from "react-bootstrap";
 
 import "./tables.css";
 
-export const Tables = ({ feed, rssfeed }) => {
+export const Tables = ({ feed, feedol }) => {
   return (
     <div className="table">
+      <ReactBootStrap.Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Link</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{feedol.title}</td>
+            <td>{feedol.link}</td>
+            <td>{feedol.updated}</td>
+          </tr>
+        </tbody>
+      </ReactBootStrap.Table>
+      <ReactBootStrap.Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Link</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{feed.title}</td>
+            <td>{feed.link}</td>
+            <td>{feed.updated}</td>
+          </tr>
+        </tbody>
+      </ReactBootStrap.Table>
+      <ReactBootStrap.Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Link</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{feed.title}</td>
+            <td>{feed.link}</td>
+            <td>{feed.updated}</td>
+          </tr>
+        </tbody>
+      </ReactBootStrap.Table>
       <ReactBootStrap.Table striped bordered hover>
         <thead>
           <tr>
