50516e090e583544e5468215b591f3caf01239c4
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-07 17:20:21 -0700

    updated tables js and css

--- client/src/components/Table/Tables.js
+++ client/src/components/Table/Tables.js
@@ -1,75 +1,55 @@
 import React from "react";
-import * as ReactBootStrap from "react-bootstrap";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 import "./tables.css";
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 
 export const Tables = ({ feed, feedol }) => {
   return (
-    <div className="table">
-      <ReactBootStrap.Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Link</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{feedol.title}</td>
-            <td>{feedol.link}</td>
-            <td>{feedol.updated}</td>
-          </tr>
-        </tbody>
-      </ReactBootStrap.Table>
-      <ReactBootStrap.Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Link</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{feed.title}</td>
-            <td>{feed.link}</td>
-            <td>{feed.updated}</td>
-          </tr>
-        </tbody>
-      </ReactBootStrap.Table>
-      <ReactBootStrap.Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Link</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{feed.title}</td>
-            <td>{feed.link}</td>
-            <td>{feed.updated}</td>
-          </tr>
-        </tbody>
-      </ReactBootStrap.Table>
-      <ReactBootStrap.Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Link</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{feed.title}</td>
-            <td>{feed.link}</td>
-            <td>{feed.updated}</td>
-          </tr>
-        </tbody>
-      </ReactBootStrap.Table>
-    </div>
+    <TableContainer component={Paper}>
+      <Table style={{ Width: 100 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
