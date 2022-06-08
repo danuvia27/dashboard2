4bc05e77db75a81dc1b96e5bde42af37776afd75
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-07 17:19:48 -0700

    Created new navbar.js and css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,44 @@
-import React from "react";
-import { AiOutlineSetting } from "react-icons/ai";
-import { IoMdNotificationsOutline } from "react-icons/io";
-import { MdOutlineDashboardCustomize } from "react-icons/md";
-import { IoMdTime } from "react-icons/io";
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
+import { IconContext } from "react-icons";
 
-function Navbar() {
+export default function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
-    <div className="navbar">
-      <div className="navbarWrapper">
-        <div className="topLeft">
-          <span className="logo">
-            Dashboard <MdOutlineDashboardCustomize />
-          </span>
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
         </div>
-        <div className="topRight">
-          <div className="topbarIconContainer">
-            <IoMdNotificationsOutline />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <AiOutlineSetting />
-          </div>
-          <div className="topbarIconContainer">
-            <IoMdTime />
-          </div>
-        </div>
-      </div>
-    </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
   );
 }
-
-export default Navbar;
