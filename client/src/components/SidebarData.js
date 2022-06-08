a094102d07ac5c91ddf30f5e5b79728be1998e79
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-07 17:21:02 -0700

    Created SidebarData.js

---
+++ client/src/components/SidebarData.js
@@ -0,0 +1,36 @@
+import React from "react";
+import * as AiIcons from "react-icons/ai";
+import * as FiIcons from "react-icons/fi";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiOutlineHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Team",
+    path: "/team",
+    icon: <AiIcons.AiOutlineTeam />,
+    cName: "nav-text",
+  },
+  {
+    title: "Outages",
+    path: "/outages",
+    icon: <AiIcons.AiOutlineThunderbolt />,
+    cName: "nav-text",
+  },
+  {
+    title: "Settings",
+    path: "/setttings",
+    icon: <FiIcons.FiSettings />,
+    cName: "nav-text",
+  },
+  {
+    title: "Support",
+    path: "/support",
+    icon: <FiIcons.FiHelpCircle />,
+    cName: "nav-text",
+  },
+];
