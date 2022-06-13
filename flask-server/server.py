e400a798093f3d160d04180e3190f8f8da35c2e5
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-13 08:10:44 -0700

    Adding def json_data to parse URL

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,7 @@
 import feedparser
-from flask import Flask ,jsonify
+import requests
+import json
+from flask import Flask ,jsonify, render_template
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app, resources={r"/api/*":{"origins":"*"}})
@@ -7,6 +9,7 @@ app.config['CORS HEADERS'] = 'Content-Type'
 URLS =['https://status.ui.com/history.rss',
 'http://status.us.onelogin.com/pages/538511e2ce5cb97537000144/rss']
 
+data = "http://status.aws.amazon.com/data.json"
 
 @app.route("/feed", methods=['POST','GET'])
 @cross_origin()
@@ -18,7 +21,7 @@ def get_feeds_ui():
     return jsonify(title=first_article.title,
          link=first_article.link, 
          updated=first_article.updated)
-         
+
 @app.route("/feed_ol", methods=['POST','GET'])
 def get_feeds_ol():
     feed = feedparser.parse(URLS[1])  
@@ -27,12 +30,19 @@ def get_feeds_ol():
     return jsonify(title=first_article.title,
     link=first_article.link,
     updated=first_article.updated)
+
+
+
+@app.route("/json_data", methods=['POST','GET'])
+def json_data():
+    req = requests.get('http://status.aws.amazon.com/data.json')
+    data = json.loads(req.content)
+    return  render_template('index.html', data=data)
+   
   
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
 
 
 
