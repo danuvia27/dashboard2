352d704e35ccd04d84b836037b0058ba91fa6472
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-23 14:12:10 -0700

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Tables } from "./components/Table/Tables";
 import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Cards from "./components/Cards";
+import Home from "./pages/Home";
+import Team from "./pages/Team";
+import Outages from "./pages/Outages";
 
 function App() {
   const [feed, setFeed] = useState([]);
@@ -41,16 +46,29 @@ function App() {
     );
   }, []);
 
+  const refresh = () => {
+    window.location.reload();
+  };
+
+  const location = useLocation();
+  console.log("pathname", location.pathname);
   return (
     <>
-      <Router>
-        <Navbar />
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/team" element={<Team />} />
+          <Route exact path="/outages" element={<Outages />} />
+        </Route>
+      </Routes>
+
       <Tables
         feed={feed}
         feedol={feedol}
         jsonData={jsonData}
         azureData={azureData}
+        refresh={refresh}
       />
     </>
   );
