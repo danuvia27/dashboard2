52975dcc1828f293fdef5e9db34c0f9dec0ccb1a
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-23 14:13:44 -0700

    Updated Navbar.js and css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
+import { BsArrowClockwise } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import { FiAlertOctagon } from "react-icons/fi";
 import { MdOutlineDashboardCustomize } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
-import { Nav } from "react-bootstrap";
 
-export default function Navbar() {
+export default function Navbar(getAllFour) {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
@@ -16,31 +18,30 @@ export default function Navbar() {
     <>
       <IconContext.Provider value={{ color: "#FC5750" }}>
         <div className="navbar">
-          <Link to="#" className="menu-bars">
-            <FaIcons.FaBars onClick={showSidebar} />
+          <Link to="/" className="menu-bars">
+            <FaIcons.FaBars />
           </Link>
-          <Nav className="justify-content-end" activeKey="/home">
-            <Nav.Item>
-              <Nav.Link href="/home">Active</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-1">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-2">Link</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="disabled" disabled>
-                Disabled
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
+          <ul className="nav justify-content-end">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                <FiAlertOctagon />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                <FiSettings />
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a></a>
+            </li>
+          </ul>
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
+          <ul className="nav-menu-items">
             <li className="navbar-toggle">
               <div className="dash"> Dashboard</div>
-              <Link to="#" className="menu-bars">
+              <Link to="/" className="menu-bars">
                 <MdOutlineDashboardCustomize />{" "}
               </Link>
             </li>
