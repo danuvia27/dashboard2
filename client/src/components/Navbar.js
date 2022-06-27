1e186a6581eb36caab43e1b436c0b12ca61c5a9c
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-27 14:58:11 -0700

    Updated Navbar with current date

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import * as FaIcons from "react-icons/fa";
-import { BsArrowClockwise } from "react-icons/bs";
 import { FiSettings } from "react-icons/fi";
 import { FiAlertOctagon } from "react-icons/fi";
 import { MdOutlineDashboardCustomize } from "react-icons/md";
@@ -8,6 +6,7 @@ import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
+import CurrentDate from "./CurrentDate";
 
 export default function Navbar(getAllFour) {
   const [sidebar, setSidebar] = useState(false);
@@ -18,10 +17,14 @@ export default function Navbar(getAllFour) {
     <>
       <IconContext.Provider value={{ color: "#FC5750" }}>
         <div className="navbar">
-          <Link to="/" className="menu-bars">
-            <FaIcons.FaBars />
-          </Link>
+          <Link to="/" className="menu-bars"></Link>
           <ul className="nav justify-content-end">
+            <li className="nav-date">
+              <a>
+                {" "}
+                <CurrentDate />{" "}
+              </a>
+            </li>
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">
                 <FiAlertOctagon />
@@ -31,9 +34,6 @@ export default function Navbar(getAllFour) {
               <Link className="nav-link" to="/">
                 <FiSettings />
               </Link>
-            </li>
-            <li className="nav-item">
-              <a></a>
             </li>
           </ul>
         </div>
