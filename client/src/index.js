d4a8f7e642c4476c294092842c0e5f8f9cac2f59
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2022-06-29 15:31:41 -0700

    Updated index.js with browserRouter

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
